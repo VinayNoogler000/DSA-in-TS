4af32c1db1b0451cbe98dec249aa19e86bd70390
VinayNoogler000 <vinaytambey000@gmail.com>
2026-05-15 23:36:05 +0530

    Refactor by uncommenting the source-code to make it functional, and every approach of taking input runs perfectly fine when running the file at once.

--- src/input.ts
+++ src/input.ts
@@ -6,27 +6,27 @@ const rl: readline.Interface = readline.createInterface({
   output: process.stdout,
 });
 
-// const singeLine: string = await rl.question('Enter a number: '); // use to take a single line of input
-// console.log(singeLine);
-// rl.close(); // closes the interface when done to stop the terminal from constantly listening for input, and preventing hanging of terminal.
+const singeLine: string = await rl.question('Enter a number: '); // use to take a single line of input
+console.log(singeLine);
+rl.close(); // closes the interface when done to stop the terminal from constantly listening for input, and preventing hanging of terminal.
 
-// rl.on("line", (input: string) => { // use to take a multiple lines of input
-//     if (input.toLowerCase() === "exit") {
-//         rl.close();
-//         return;
-//     }
-//     console.log(`You Typed: ${input}`);
-// });
+rl.on("line", (input: string) => { // use to take a multiple lines of input
+    if (input.toLowerCase() === "exit") {
+        rl.close();
+        return;
+    }
+    console.log(`You Typed: ${input}`);
+});
 
 
 // Easier Way for String inputs (compatible with Bun run-time env only) - NOT Recommended
 // BUT can cause "double input" errors (forcing us to input value twice, due to mis-interpration of "Enter" key)
-// const name = prompt("What is your name?\n");
-// console.log(`Hello, ${name}!`);
+const name = prompt("What is your name?\n");
+console.log(`Hello, ${name}!`);
 
 
 // process.argv: To pass input as cmd-line arguments, when executing a file (js/ts) from Node/Bun
-// console.log(process.argv); 
+console.log(process.argv); 
 //an array of arguments, whose 0th idx is path of run-time env, and 1st idx is path of script file (js/ts), and following indexes stores the passed arguments.
 // For ex: in this cmd-line `bun src/input.ts hello I am Vinay` the args are "hello", "I", "am", "Vinay"
 
