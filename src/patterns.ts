49b93e1e00008e3458476c5596f737fdf69b7d18
VinayNoogler000 <vinaytambey000@gmail.com>
2026-05-15 23:38:07 +0530

    Refactor by uncommenting the code of printing patterns making sure that every pattern is being printed when the entire file is excuted at once,, and add  proper comments and spacing between each of them for  better readability and understanding.

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,39 +1,54 @@
-// for (let i:number = 0; i<5; i++) {
-//     console.log("* * * * *");
-// }
-/*
+// NOTE: You can run the entire file at once by using node/bun (or other runtime envs) to print all the patterns and confirm whether the code is working fine or not:
 
-Prints Pattern:
+
+/*
+Pattern 1:
 * * * * *
 * * * * *
 * * * * *
 * * * * *
 * * * * *
 */
+console.log("Pattern 1");
+for (let i:number = 0; i<5; i++) {
+    console.log("* * * * *");
+}
+console.log("\n");
 
 
 
-// let rows:number = 5;
-// for (let i:number = 0; i < rows; i++) {
-//     let p:string = ""; //pattern
-
-//     for (let j:number = 1; j <= (rows-i); j++) {
-//         p = p.concat(j.toString());
-//     }
-//     console.log(p);
-// }
 
 /*
-Prints Pattern:
+Pattern 2:
 12345
 1234
 123
 12
 1
 */
+console.log("Pattern 2:");
+let rows1:number = 5;
+for (let i:number = 0; i < rows1; i++) {
+    let pattern1:string = ""; //pattern
+
+    for (let j:number = 1; j <= (rows1-i); j++) {
+        pattern1 = pattern1.concat(j.toString());
+    }
+    console.log(pattern1);
+}
+console.log("\n");
 
 
 
+
+/*
+Pattern 3:
+    *
+   ***
+  *****
+ *******
+*********
+*/
 /*
 LOGIC:
 i -> k
@@ -43,31 +58,34 @@ i -> k
 4 = 7
 5 = 9
 */
-// let rows:number = 5;
-// for (let i:number = 1; i<=rows; i++) {
-//     let spaces:string = '';
-//     for (let j:number = 1; j <= (rows-i); j++) {
-//         spaces += ' ';
-//     }
+console.log("Pattern 3");
+let rows2:number = 5;
+for (let i:number = 1; i<=rows2; i++) {
+    let spaces:string = '';
+    for (let j:number = 1; j <= (rows2-i); j++) {
+        spaces += ' ';
+    }
 
-//     let stars:string = '';
-//     for (let k:number = 1; k<= (i*2-1); k++) {
-//         stars += '*';
-//     }
+    let stars:string = '';
+    for (let k:number = 1; k<= (i*2-1); k++) {
+        stars += '*';
+    }
 
-//     console.log(`${spaces}${stars}`);
-// }
+    console.log(`${spaces}${stars}`);
+}
+console.log("\n");
+
+
+
+
 /*
-Prints Pattern:
-    *
-   ***
-  *****
- *******
-*********
+Pattern:
+ *********
+  *******
+   *****
+    ***
+     *
 */
-
-
-
 /*
 LOGIC:
 i -> k
@@ -77,104 +95,77 @@ i -> k
 2 = 3
 1 = 1
 */
-// let rows: number = 5, pattern:string;
-// for (let i:number = rows; i>=1; i--) {
-//     // print spaces
-//     pattern = "";
-//     for (let j:number = 0; j<=(rows-i); j++) {
-//         pattern += ' ';
-//     }
+console.log("Pattern 4:");
+let rows3: number = 5;
+let pattern3:string;
+for (let i:number = rows3; i>=1; i--) {
+    // print spaces
+    pattern3 = "";
+    for (let j:number = 0; j<=(rows3-i); j++) {
+        pattern3 += ' ';
+    }
 
-//     // print stars
-//     for (let k:number = 0; k<(i*2-1); k++) {
-//         pattern = pattern.concat('*');
-//     }
+    // print stars
+    for (let k:number = 0; k<(i*2-1); k++) {
+        pattern3 = pattern3.concat('*');
+    }
 
-//     console.log(pattern);
-// }
+    console.log(pattern3);
+}
+console.log("\n");
+
+
+
+
 /*
-Prints Pattern:
+Pattern 5:
+     *
+    ***
+   *****
+  *******
+ *********
  *********
   *******
    *****
     ***
      *
 */
-
-
-
-
-// let rows:number = 10, hOfRows:number = (rows/2), pattern:string;
-// for(let i:number = 0; i<hOfRows; i++) { // Print Upper Triangle
-//     pattern = '';
+console.log("Pattern 5:");
+let rows4:number = 10, hOfRows4:number = (rows4/2), pattern4:string;
+for(let i:number = 0; i<hOfRows4; i++) { // Print Upper Triangle
+    pattern4 = '';
     
-//     // print spaces
-//     for (let j:number = 0; j<(hOfRows-i); j++) {
-//         pattern += ' ';
-//     }
+    // print spaces
+    for (let j:number = 0; j<(hOfRows4-i); j++) {
+        pattern4 += ' ';
+    }
     
-//     // print stars
-//     for (let k:number = 0; k<=(i*2); k++) {
-//         pattern = pattern.concat('*');
-//     }
-//     console.log(pattern);
-// }
-// for (let i:number = hOfRows; i>0; i--) { // Print Bottom Triangle
-//     pattern = '';
+    // print stars
+    for (let k:number = 0; k<=(i*2); k++) {
+        pattern4 = pattern4.concat('*');
+    }
+    console.log(pattern4);
+}
+for (let i:number = hOfRows4; i>0; i--) { // Print Bottom Triangle
+    pattern4 = '';
     
-//     // print spaces
-//     for (let j:number = hOfRows-i; j>=0; j--) {
-//         pattern += ' ';
-//     }
+    // print spaces
+    for (let j:number = hOfRows4-i; j>=0; j--) {
+        pattern4 += ' ';
+    }
         
-//     // print stars
-//     for (let k:number = 1; k<(i*2); k++) {
-//         pattern = pattern.concat('*');
-//     }    
-//     console.log(pattern);
-// }
+    // print stars
+    for (let k:number = 1; k<(i*2); k++) {
+        pattern4 = pattern4.concat('*');
+    }    
+    console.log(pattern4);
+}
+console.log("\n");
+
+
+
 /*
-Prints Pattern:
-     *
-    ***
-   *****
-  *******
- *********
- *********
-  *******
-   *****
-    ***
-     *
-*/
-
-
-
-
-// const rows: number = 9;
-// let firstHalfRows = Math.floor(rows/2), nextHalfRows = Math.ceil(rows/2), pattern:string;
-
-// print 1st Half of Pattern
-// for (let i:number = 0; i<firstHalfRows; i++) {
-//     pattern = '';
-
-//     for (let j:number = 0; j<=i; j++) {
-//         pattern += '*';
-//     }
-
-//     console.log(pattern);
-// }
-
-// print 2nd Half of Pattern
-// for (let i:number = nextHalfRows; i>0; i--) {
-//     pattern = '';
-
-//     for (let j:number = 0; j<i; j++) {
-//         pattern += '*';
-//     }
-//     console.log(pattern);
-// }
-/*
-Prints Pattern:
+Pattern 6:
 *
 **
 ***
@@ -185,61 +176,93 @@ Prints Pattern:
 **
 *
 */
+console.log("Pattern 6:");
+const rows5: number = 9;
+let firstHalfRows5 = Math.floor(rows5/2), nextHalfRows5 = Math.ceil(rows5/2), pattern5:string;
+
+// print 1st Half of Pattern
+for (let i:number = 0; i<firstHalfRows5; i++) {
+    pattern5 = '';
+
+    for (let j:number = 0; j<=i; j++) {
+        pattern5 += '*';
+    }
+
+    console.log(pattern5);
+}
+
+// print 2nd Half of Pattern
+for (let i:number = nextHalfRows5; i>0; i--) {
+    pattern5 = '';
+
+    for (let j:number = 0; j<i; j++) {
+        pattern5 += '*';
+    }
+    console.log(pattern5);
+}
+console.log("\n");
 
 
 
 
-// const rows: number = 5;
-// let pattern:string;
-// for (let i:number = 0; i<rows; i++) {
-//     pattern = ''
-    
-//     for (let j:number = 0; j<=i; j++) {
-//         if ((i === 0 && j === 0) || (i+j) % 2 != 0) { // prints '0' when at 0th position or i+j == odd
-//             pattern += " 0 ";
-//         }
-//         else if ( (i+j) % 2 === 0) { // prints '1' when i+j == even
-//             pattern += " 1 ";
-//         }
-//     }
-
-//     console.log(pattern);
-// }
 /*
-Prints Pattern:
+Pattern 7:
 0 
 0  1 
 1  0  1 
 0  1  0  1 
 1  0  1  0  1 
 */
-
-// const rows:number = 4;
-// let pattern:string;
-// for (let i:number = 1, spaces:number = (rows*2-2); i<=rows; i++, spaces -= 2) {
-//     pattern = '';
+console.log("Pattern 7:");
+const rows6: number = 5;
+let pattern6:string;
+for (let i:number = 0; i<rows6; i++) {
+    pattern6 = ''
     
-//     // prints 1st Part Number
-//     for (let j:number = 1; j<=i; j++) {
-//         pattern += j;
-//     }
+    for (let j:number = 0; j<=i; j++) {
+        if ((i === 0 && j === 0) || (i+j) % 2 != 0) { // prints '0' when at 0th position or i+j == odd
+            pattern6 += " 0 ";
+        }
+        else if ( (i+j) % 2 === 0) { // prints '1' when i+j == even
+            pattern6 += " 1 ";
+        }
+    }
 
-//     // prints Mid spaces
-//     for (let k:number = 1; k<=spaces; k++) {
-//         pattern += ' ';
-//     }
+    console.log(pattern6);
+}
+console.log("\n");
 
-//     // print Last Part Number
-//     for (let l:number = i; l>=1; l--) {
-//         pattern += l;
-//     }
 
-//     console.log(pattern);
-// }
+
+
 /*
-Prints Pattern:
+Pattern:
 1      1
 12    21
 123  321
 12344321
 */
+console.log("Pattern 8:");
+const rows7:number = 4;
+let pattern7:string;
+for (let i:number = 1, spaces:number = (rows7*2-2); i<=rows7; i++, spaces -= 2) {
+    pattern7 = '';
+    
+    // prints 1st Part Number
+    for (let j:number = 1; j<=i; j++) {
+        pattern7 += j;
+    }
+
+    // prints Mid spaces
+    for (let k:number = 1; k<=spaces; k++) {
+        pattern7 += ' ';
+    }
+
+    // print Last Part Number
+    for (let l:number = i; l>=1; l--) {
+        pattern7 += l;
+    }
+
+    console.log(pattern7);
+}
+console.log("\n");
