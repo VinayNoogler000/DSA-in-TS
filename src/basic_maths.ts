7526476256b88a4eb4d12723c1db990a6f0c6243
VinayNoogler000 <vinaytambey000@gmail.com>
2026-06-01 14:19:09 +0530

    Add function to count the number of digits in an integer.

---
+++ src/basic_maths.ts
@@ -0,0 +1,12 @@
+// Q1: Given an integer N, return the number of digits in N.
+function countDigits( n:number=12345 ):number { // output = 5
+    let count = 0, lastDigit:number;
+
+    while (n > 0) {
+        lastDigit = n % 10;
+        count++;
+        n = Math.floor(n/10); // removing last digit
+    }
+    
+    return count;
+}
