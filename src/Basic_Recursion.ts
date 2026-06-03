09c38269439337628e8db71ebf31d62fee23e9c6
VinayNoogler000 <vinaytambey000@gmail.com>
2026-06-03 14:34:32 +0530

    - Add recursive function to print numbers from 1 to N. - Add all recursive functions to BasicRecursion class in Basic_Recursion.ts.

--- src/Basic_Recursion.ts
+++ src/Basic_Recursion.ts
@@ -1,11 +1,29 @@
-// Problem 1: Given an integer N, write a program to print your name N times.
-function printNameNthTimes(name:string, n:number): void { // Both TC & SC == O(n), cuz Nth times recursive func() is called and added to Call Stack as stack-frames.
-    if (n === 0) { // Base Condition 
-        return;
-    }
-
-    console.log("Name:", name); // Operation to Perform
+export default class BasicRecursion {
     
-    printNameNthTimes(name, --n); // Next Recursive Call
+    // Problem 1: Given an integer N, write a program to print your name N times.
+    public static printNameNthTimes(name:string, n:number=5): void { // Both TC & SC == O(n), cuz Nth times recursive func() is called and added to Call Stack as stack-frames.
+        if (n === 0) { // Base Condition 
+            return;
+        }
+        
+        console.log("Name:", name); // Operation to Perform
+        
+        BasicRecursion.printNameNthTimes(name, --n); // Next Recursive Call
+    }
+    
+    
+    
+    
+    // Problem 2: Given an integer N, write a program to print numbers from 1 to N.
+    public static printNumsFromOneTillN(n:number=5, count:number=1): void { // Both TC & SC == O(n)
+        if (count > n ) { // Base Condition
+            return;
+        }
+        
+        console.log(count);
+        BasicRecursion.printNumsFromOneTillN(n, ++count);
+    }
 }
-printNameNthTimes("Vinay", 5);
+
+// BasicRecursion.printNameNthTimes("Vinay", 5);
+BasicRecursion.printNumsFromOneTillN();
