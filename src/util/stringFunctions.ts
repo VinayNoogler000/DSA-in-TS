e85ea83522a74f4663572333171c2d40f9054cb8
VinayNoogler000 <vinaytambey000@gmail.com>
2026-06-08 13:37:00 +0530

    - Add various string functions to check character types in string: isStringHasLowerCase, isStringHasUpperCase, isStringHasNums, isStringHasSymbols. - Add getMaxLengthOfString function to calculate the maximum number of unique characters in a string for O(1) lookups.

---
+++ src/util/stringFunctions.ts
@@ -0,0 +1,72 @@
+function isStringHasLowerCase(str:string):boolean {
+    for (let ch of str) {
+        if (ch === ch.toLowerCase()) return true;
+    }
+
+    return false;
+}
+
+function isStringHasUpperCase(str:string):boolean {
+    for (let ch of str) {
+        if (ch === ch.toUpperCase()) return true;
+    }
+
+    return false;
+}
+
+function isStringHasNums(str:string):boolean {
+    const numericCharSet = new Set<string>(); // O(1) SC, 'cause it always stores only 9 characters
+
+    // Iterate and Add Numeric Chars to Set
+    for (let i=0; i<=9; i++) { // O(1) TC
+        numericCharSet.add( i.toString() );
+    }
+
+    // Iterate Str and Find out if all the chars are numbers or not
+    for (let ch of str) { // O(n), where n is length of string 'str'
+        if ( numericCharSet.has(ch) ) return true;
+    }
+
+    return false;
+}
+
+function isStringHasSymbols(str:string):boolean {
+    return /[^a-zA-Z0-9]/.test(str); // means, find at least one char in string which is not in range "a - z", "A - Z", or "0 - 9"
+}
+
+// This Function returns the maximum number of unique characters (length) that a structure can store for lookups at constant O(1) time.
+function getMaxLengthOfString(str:string): number {
+    const isStrHasLowerChars = isStringHasLowerCase(str); // a to z
+    const isStrHasUpperChars = isStringHasUpperCase(str); // A to Z
+    const isStrHasNumChars = isStringHasNums(str); // 0 - 9
+    const isStrHasSymbolicChars = isStringHasSymbols(str); // All Symbols (Printable & Non-Printable Chars)
+
+    if ( isStrHasLowerChars && isStrHasUpperChars && isStrHasNumChars && isStrHasSymbolicChars) {
+        return 128;
+    }
+    else if (isStrHasLowerChars && isStrHasUpperChars && isStrHasNumChars) {
+        return 62;
+    }
+    else if (isStrHasLowerChars && isStrHasUpperChars) {
+        return 52;
+    }
+    else if (isStrHasLowerChars || isStrHasUpperChars) {
+        return 26;
+    }
+    else if (isStrHasNumChars) {
+        return 10;
+    }
+    else if (isStrHasSymbolicChars) {
+        return 64;
+    }
+
+    return 0;
+}
+
+export {
+    isStringHasLowerCase,
+    isStringHasUpperCase,
+    isStringHasNums,
+    isStringHasSymbols,
+    getMaxLengthOfString,
+}
