a2e846b9e0b2db69c5aed679722d224e64d7e2e0
VinayNoogler000 <vinaytambey000@gmail.com>
2026-06-08 13:37:49 +0530

    Add manual conversion of a single digit number to its corresponding character representation in ASCII.

--- src/util/characterFunctions.ts
+++ src/util/characterFunctions.ts
@@ -13,3 +13,8 @@ export function decrementCharBy1(char:string): string {
     }
     return String.fromCharCode(char.charCodeAt(0) - 1); 
 }
+
+export function convertNumDigitToStringChar(num:number):string {
+    let asciiVal = num + 48; // ASCII Code of '0' is 48, and '9' is 57
+    return String.fromCharCode(asciiVal);
+}
