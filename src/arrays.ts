6a6081a4f83c25475c37782fc29f03c174e9e852
VinayNoogler000 <vinaytambey000@gmail.com>
2026-06-19 18:10:31 +0530

    Add 3 different solutions: brute-force, better approach using array.sort(), and better approach without sorting to find second smallest and largest elements in an array

--- src/arrays.ts
+++ src/arrays.ts
@@ -51,4 +51,114 @@ function largestElementInArrOA(nums:number[]=[4,3,2,1], n=nums.length):number {
     return max;
 }
 
-process.stdout.write(largestElementInArrOA() + '');
+
+
+
+// Q3: Given an array, find the second smallest and second largest element in the array. Print ‘-1’ in the event when either of them doesn’t exist.
+
+// Brute-Force Approach (using Inbuilt Array.sort() ) - TC O(N*logN) and SC O(1)
+function find2ndSmallestAndLargestNums(nums:number[]=[4,3,2,1], n=nums.length):number[] {
+    if (n <= 1) return [-1, -1];
+
+    nums.sort((a, b) => a-b); 
+
+    // Basic Approach (doesn't cover duplicate-elements test-case)
+    // return [nums[1]!, nums[n-2]!]; // [second-Smallest, second-Largest]
+
+    // Loop to Find the Second Smallest Element in the Array
+    let i:number, secondSmallest = -1;
+    for (i=1; i<n; i++) {
+        if (nums[i] !== nums[i-1]) {
+            secondSmallest = nums[i]!;
+            break;
+        }
+    }
+
+    // Loop to Find the Second Largest Element in the Array
+    let j:number, secondLargest = -1;
+    for (j=n-2; j>=0; j--) {
+        if (nums[j] !== nums[j+1]) {
+            secondLargest = nums[j]!;
+            break;
+        }
+    }
+
+    return [secondSmallest, secondLargest]; // if array consists of same duplicate values, then result will be [-1, -1], means no second smallest and largest elements exists in the array.
+}
+
+// Better Approach (using Single Loop after Array.sort() ) - TC O(N*logN) and SC O(1)
+function find2ndSmallestAndLargestNumsBA(nums=[4,3,2,1], n=nums.length):number[] {
+    if (n <= 1) return [-1, -1];
+
+    nums.sort((a, b) => a-b); 
+
+    let secondSmallest = -1, secondLargest = -1;
+    let i = 1, j = n-2;
+    
+    // Single Loop to Traverse and Find the Second Smallest & Largest Elements
+    while (i < n || j >= 0) {
+        if(i < n ) {
+            if (nums[i] !== nums[i-1]) {
+                secondSmallest = nums[i]!;
+                i = n; // to prevent waste of computational power by performing redundant ops.
+            }
+            else i++;
+        }
+
+        if (j >= 0) {
+            if (nums[j] !== nums[j+1]) {
+                secondLargest = nums[j]!;
+                j = -1; // to prevent wastage of computational power by performing redundant ops.
+            }
+            else if (j >= 0) j--; 
+        }
+
+        // OR, Easier to Understand, but the no. of computation is higher than previous
+        // if (secondSmallest === -1 && nums[i] !== nums[i-1]) {
+        //     secondSmallest = nums[i]!;
+        // }
+
+        // if (secondLargest === -1 && nums[j] !== nums[j+1]) {
+        //     secondLargest = nums[j]!;
+        // }
+
+        // i++; j--;
+    }
+
+    return [secondSmallest, secondLargest]
+}
+
+// Better Approach (without Any Sorting) - TC O(N) and SC O(1)
+function find2ndSmallestAndLargestNumsMBA(nums=[4,3,2,1], n=nums.length):number[] {
+    if (n <= 1) return [-1, -1];
+
+    // Find the Smallest and Largest Elements in the Array
+    let smallest = Infinity, largest = -Infinity;
+    for (let num of nums) { // O(N)
+        if (num < smallest) {
+            smallest = num;
+        }
+
+        if (num > largest) {
+            largest = num;
+        }
+    }
+
+    // Find the Second Smallest and Largest Elements in the Array
+    let secondSmallest = Infinity, secondLargest = -Infinity;
+    for (let num of nums) { // O(N)
+        if (num > smallest && num < secondSmallest ) {
+            secondSmallest = num;
+        }
+
+        if (num < largest && num > secondLargest) {
+            secondLargest = num;
+        }
+    }
+
+    return [
+        secondSmallest === Infinity ? -1 : secondSmallest, 
+        secondLargest === -Infinity ? -1 : secondLargest
+    ]
+} 
+process.stdout.write(find2ndSmallestAndLargestNumsMBA([1, 1, 2, 3, 3]) + "");
